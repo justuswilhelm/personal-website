bc1fa3b7ec0d6fb8d3988acd074c58127963a459
Justus Perlwitz <mail@justusperlwitz.com>
2014-09-20 13:47:18 +0200

    Basic structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,42 @@
+var concat = require('gulp-concat');
 var gulp = require('gulp');
-var mainBowerFiles = require('main-bower-files');
+var jade = require('gulp-jade');
+var bower = require('gulp-bower');
+var rimraf = require('gulp-rimraf');
 
-gulp.task('bower', function() {
-  return gulp.src(mainBowerFiles(), {
-    base: 'bower_components'
-  }).pipe(gulp.dest('public/lib'));
+var SRC = {
+    jade: 'private/*.jade',
+    css: 'private/css/*.css'
+};
+
+var TARGET = {
+    public: 'public/',
+    css: 'public/css'
+};
+
+gulp.task('jade', function () {
+    return gulp.src(SRC.jade)
+         .pipe(jade())
+         .pipe(gulp.dest(TARGET.public));
 });
+
+gulp.task('css', function () {
+    return gulp.src(SRC.css)
+               .pipe(concat('main.css'))
+               .pipe(gulp.dest(TARGET.css));
+});
+
+gulp.task('clean', function () {
+    return gulp.src(TARGET.public)
+               .pipe(rimraf());
+});
+
+gulp.task('watch', function () {
+    gulp.watch(SRC.jade, ['jade']);
+    gulp.watch(SRC.css, ['css']);
+});
+
+gulp.task('default', [
+    'jade',
+    'css'
+]);
