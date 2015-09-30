f88acf69de6fe502811f1395c87608765f55e74f
Justus Perlwitz <mail@justusperlwitz.com>
2015-09-30 20:04:55 +0200

    ADD: Redis setup

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from flask import (
     Flask,
     render_template,
@@ -5,6 +6,29 @@ from flask import (
 
 application = Flask(__name__)
 
+BlogEntry = namedtuple('BlogEntry', ['date', 'title', 'content'])
+
+
+def get_blog_entry(entry):
+    return ""
+
+
+def get_blog_entries():
+    return []
+
+
 @application.route("/")
 def index():
     return render_template("index.html")
+
+
+@application.route("/blog/<entry>/")
+def blog_entry(entry):
+    blog_entry = get_blog_entry(entry)
+    return render_template("blog_entry.html", entry=blog_entry)
+
+
+@application.route("/blog/")
+def blog_index():
+    blog_entries = get_blog_entries()
+    return render_template("blog_index.html", entries=blog_entries)
