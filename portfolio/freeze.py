633cbdaab49fb4c9b059d4208c31c49fe413f0c9
Justus Perlwitz <mail@justusperlwitz.com>
2017-11-26 20:38:48 +0100

    Switch to jekyll

--- portfolio/freeze.py
+++
@@ -1,9 +0,0 @@
-"""."""
-from flask_frozen import Freezer
-from . import application
-
-application.config['FREEZER_DEFAULT_MIMETYPE'] = 'text/html'
-application.config['FREEZER_STATIC_IGNORE'] = (
-    '.DS_Store',
-)
-freezer = Freezer(application)
