63e587190d8bb571377d7e1af455dcf4f4f430bf
Justus Perlwitz <mail@justusperlwitz.com>
2019-02-18 01:14:14 +0900

    Format source

--- PandocFilterGraphviz.hs
+++ PandocFilterGraphviz.hs
@@ -32,25 +32,25 @@
 
 module PandocFilterGraphviz where
 
-import Crypto.Hash
-import Control.Monad (unless)
+import           Control.Monad          (unless)
+import           Crypto.Hash
 
-import Data.ByteString (ByteString)
-import Data.Byteable (toBytes)
-import qualified Data.ByteString.Char8 as C8
+import           Data.Byteable          (toBytes)
+import           Data.ByteString        (ByteString)
 import qualified Data.ByteString.Base16 as B16
+import qualified Data.ByteString.Char8  as C8
 
-import qualified Data.Map.Strict as M
-import Data.Text as T
-import Data.Text.Encoding as E
+import qualified Data.Map.Strict        as M
+import           Data.Text              as T
+import           Data.Text.Encoding     as E
 
-import System.FilePath
-import System.Directory
-import System.Exit
-import System.Process (system)
+import           System.Directory
+import           System.Exit
+import           System.FilePath
+import           System.Process         (system)
 
-import Text.Pandoc
-import Text.Pandoc.JSON
+import           Text.Pandoc
+import           Text.Pandoc.JSON
 
 (¤) :: Text -> Text -> Text
 (¤) = T.append
@@ -62,87 +62,90 @@ sha :: Text -> Text
 sha = E.decodeUtf8 . hexSha3_512 . B16.encode . E.encodeUtf8
 
 fileName4Code :: Text -> Text -> Maybe Text -> FilePath
-fileName4Code name source ext =
-    filename
-    where
-        dirname = name ¤ "-images"
-        shaN = sha source
-        barename = shaN ¤ (case ext of
-            Just e -> "." ¤ e
-            Nothing -> "")
-        filename = T.unpack dirname </> T.unpack barename
+fileName4Code name source ext = filename
+  where
+    dirname = name ¤ "-images"
+    shaN = sha source
+    barename =
+      shaN ¤
+      (case ext of
+         Just e  -> "." ¤ e
+         Nothing -> "")
+    filename = T.unpack dirname </> T.unpack barename
 
-getCaption :: M.Map Text Text -> (Text,Text)
-getCaption m = case M.lookup "caption" m of
-    Just cap -> (cap,"fig:")
-    Nothing -> ("","")
+getCaption :: M.Map Text Text -> (Text, Text)
+getCaption m =
+  case M.lookup "caption" m of
+    Just cap -> (cap, "fig:")
+    Nothing  -> ("", "")
 
 renderDot1 :: FilePath -> IO FilePath
-renderDot1 src =
-    renderDot src dst >> return dst
-    where
+renderDot1 src = renderDot src dst >> return dst
+  where
     dst = (dropExtension src) <.> "svg"
 
 renderDot :: FilePath -> FilePath -> IO ExitCode
 renderDot src dst =
-  system $
-    Prelude.unwords [ "dot"
-                    , "-Tsvg"
-                    , "-o" ++ show dst
-                    , show src ]
+  system $ Prelude.unwords ["dot", "-Tsvg", "-o" ++ show dst, show src]
 
 graphviz :: Block -> IO Block
 graphviz cblock@(CodeBlock (id, classes, attrs) content) =
-    if "graphviz" `elem` classes then do
-        ensureFile dest >> writeFile dest content
-        img <- renderDot1 dest
-        ensureFile img
-        return $ Para [Image (id,classes,attrs) [Str $ T.unpack caption] ("/" </> img, T.unpack caption)]
+  if "graphviz" `elem` classes
+    then do
+      ensureFile dest >> writeFile dest content
+      img <- renderDot1 dest
+      ensureFile img
+      return $
+        Para
+          [ Image
+              (id, classes, attrs)
+              [Str $ T.unpack caption]
+              ("/" </> img, T.unpack caption)
+          ]
     else return cblock
-    where
-        dest = fileName4Code "graphviz" (T.pack content) (Just "dot")
-        ensureFile fp =
-            let dir = takeDirectory fp in
-            createDirectoryIfMissing True dir >> doesFileExist fp >>=
-                \exist ->
-                unless exist $ writeFile fp ""
-        toTextPairs = Prelude.map (\(f,s) -> (T.pack f,T.pack s))
-        m = M.fromList $ toTextPairs $ attrs
-        (caption, typedef) = getCaption m
+  where
+    dest = fileName4Code "graphviz" (T.pack content) (Just "dot")
+    ensureFile fp =
+      let dir = takeDirectory fp
+       in createDirectoryIfMissing True dir >> doesFileExist fp >>= \exist ->
+            unless exist $ writeFile fp ""
+    toTextPairs = Prelude.map (\(f, s) -> (T.pack f, T.pack s))
+    m = M.fromList $ toTextPairs $ attrs
+    (caption, typedef) = getCaption m
 graphviz x = return x
 
 -- Here is some msc rendering stuff
-
 renderMsc1 :: FilePath -> IO FilePath
-renderMsc1 src =
-    renderMsc src dst >> return dst
-    where
+renderMsc1 src = renderMsc src dst >> return dst
+  where
     dst = (dropExtension src) <.> "svg"
 
 renderMsc :: FilePath -> FilePath -> IO ExitCode
 renderMsc src dst =
-  system $
-    Prelude.unwords [ "mscgen"
-                    , "-Tsvg"
-                    , "-o" ++ show dst
-                    , show src ]
+  system $ Prelude.unwords ["mscgen", "-Tsvg", "-o" ++ show dst, show src]
 
 msc :: Block -> IO Block
 msc cblock@(CodeBlock (id, classes, attrs) content) =
-  if "msc" `elem` classes then do
-    ensureFile dest >> writeFile dest content
-    img <- renderMsc1 dest
-    ensureFile img
-    return $ Para [Image (id,classes,attrs) [Str $ T.unpack caption] ("/" </> img, T.unpack caption)]
-  else return cblock
+  if "msc" `elem` classes
+    then do
+      ensureFile dest >> writeFile dest content
+      img <- renderMsc1 dest
+      ensureFile img
+      return $
+        Para
+          [ Image
+              (id, classes, attrs)
+              [Str $ T.unpack caption]
+              ("/" </> img, T.unpack caption)
+          ]
+    else return cblock
   where
     dest = fileName4Code "mscgen" (T.pack content) (Just "msc")
     ensureFile fp =
-      let dir = takeDirectory fp in
-      createDirectoryIfMissing True dir >> doesFileExist fp >>=
-        \exist ->
-          unless exist $ writeFile fp ""
-    toTextPairs = Prelude.map (\(f,s) -> (T.pack f,T.pack s))
+      let dir = takeDirectory fp
+       in createDirectoryIfMissing True dir >> doesFileExist fp >>= \exist ->
+            unless exist $ writeFile fp ""
+    toTextPairs = Prelude.map (\(f, s) -> (T.pack f, T.pack s))
     m = M.fromList $ toTextPairs $ attrs
     (caption, typedef) = getCaption m
 msc x = return x
